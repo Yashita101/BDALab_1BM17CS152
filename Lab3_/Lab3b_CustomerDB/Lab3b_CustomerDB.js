cc709e5a4fbeed8ab2beccf1e68dae500c67cb03
Yashita101 <53893563+Yashita101@users.noreply.github.com>
2020-10-12 17:32:28 +0530

    Add files via upload

---
+++ Lab3_/Lab3b_CustomerDB/Lab3b_CustomerDB.js
@@ -0,0 +1,33 @@
+show dbs
+use CustomerDB
+db
+
+db.createCollection("Customer")
+  
+//db.Customer.insertMany([
+//                        {CustID:1,AccBal:2000,AccType:"Z"},
+//                        {CustID:1,AccBal:5000,AccType:"A"},
+//                        {CustID:2,AccBal:1100,AccType:"Z"},
+//                        {CustID:2,AccBal:5000,AccType:"A"},
+//                        {CustID:3,AccBal:45000,AccType:"Z"}])
+                        
+   
+db.Customer.find().pretty();                       
+db.Customer.find({AccBal:{$gt:1200},AccType:"Z"});
+
+db.Customer.aggregate([
+                    {$group:{
+                        _id:"$CustID",
+                        min_bal:{$min:"$AccBal"},
+                        max_bal:{$max:"$AccBal"}
+                    
+                        }
+                    }
+                   ]);
+                    
+mongoexport -db CustomerDB -c
+
+                    
+                       
+                            
+                       
